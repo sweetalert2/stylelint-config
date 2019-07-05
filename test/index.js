4c0edf17fb4d5a80f3cbea9140a5eebf285b284f
Limon Monte <limonte@users.noreply.github.com>
2019-07-05 23:24:21 +0300

    be more specific in test description

--- test/index.js
+++ test/index.js
@@ -4,13 +4,26 @@ const assert = require('assert').strict
 
 const config = require('../index.js')
 
-test('stylelint configuration', async () => {
+test('declaration-block-single-line-max-declarations (stylelint-config-standard)', async () => {
   const output = await stylelint.lint({
-    code: "a { color: red; top: 0; }",
+    code: 'a { top: 0; color: red; }\n',
     config,
   })
 
+  assert.strictEqual(output.results[0].warnings.length, 1)
+  assert.strictEqual(output.results[0].warnings[0].text.trim(), 'Expected no more than 1 declaration (declaration-block-single-line-max-declarations)')
+})
+
+test('order/properties-order (stylelint-config-property-sort-order-smacss)', async () => {
+  const output = await stylelint.lint({
+    code:
+      'a {\n' +
+      '  color: red;\n' +
+      '  top: 0;\n' +
+      '}\n',
+    config,
+  })
+
+  assert.strictEqual(output.results[0].warnings.length, 1)
   assert.strictEqual(output.results[0].warnings[0].text.trim(), 'Expected "top" to come before "color" (order/properties-order)')
-  assert.strictEqual(output.results[0].warnings[1].text.trim(), 'Expected no more than 1 declaration (declaration-block-single-line-max-declarations)')
-  assert.strictEqual(output.results[0].warnings[2].text.trim(), 'Unexpected missing end-of-source newline (no-missing-end-of-source-newline)')
 })
