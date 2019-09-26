ffaec3e48f6f4dcff35886f699722e0ad16bc250
Limon Monte <limon.monte@gmail.com>
2019-09-26 12:26:11 +0300

    fix: prepare-npmrc-for-github-registry.sh -> publish-to-github-registry.sh

--- tools/publish-to-github-registry.sh
+++ tools/publish-to-github-registry.sh
@@ -4,3 +4,5 @@ echo '//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}' > .npmrc
 npm i -g json
 json -I -f package.json -e 'this.publishConfig={}'
 json -I -f package.json -e 'this.publishConfig.registry="https://npm.pkg.github.com/"'
+
+npm publish
