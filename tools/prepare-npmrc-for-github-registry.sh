0c2827e429895291ca36d9f8043c838b7ab86985
Limon Monte <limon.monte@gmail.com>
2019-09-26 12:21:28 +0300

    fix: add publishConfig to package.json before publishing to GitHub actions

--- tools/prepare-npmrc-for-github-registry.sh
+++ tools/prepare-npmrc-for-github-registry.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
 echo '//npm.pkg.github.com/:_authToken=${GITHUB_TOKEN}' > .npmrc
+
+npm i -g json
+json -I -f package.json -e 'this.publishConfig={}'
+json -I -f package.json -e 'this.publishConfig.registry="https://npm.pkg.github.com/"'
